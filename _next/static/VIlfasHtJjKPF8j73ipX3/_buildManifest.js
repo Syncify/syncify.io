982afc4a618ad01d66088a1ba73ebbf136e02a2b
dbrody <dbrody@gmail.com>
2021-01-16 20:20:30 +0000

    Deploying to gh-pages from  @ 4ff15bf580a7959ab5b0419eb10bf356224010e3 🚀

---
+++ _next/static/VIlfasHtJjKPF8j73ipX3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1110116e5e2643efcf43.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-335ca5cfdc00c619851e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
